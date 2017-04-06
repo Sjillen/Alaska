05996432e0063ad87e9f542081da281fb8acda77
Sjillen <thomas.proust@outlook.com>
2017-04-06 16:15:48 +0900

    installation de bootstrap et jquery, ajout navbar

--- app/app.php
+++ app/app.php
@@ -10,7 +10,10 @@ ExceptionHandler::register();
 //Register service providers.
 $app->register(new Silex\Provider\DoctrineServiceProvider());
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
-	'twig.path' => __DIR__.'/../views',
+	'twig.path' => __DIR__.'/../views'
+));
+$app->register(new Silex\Provider\AssetServiceProvider(), array(
+	'assets.version' => 'v1'
 ));
 
 //Register services.
